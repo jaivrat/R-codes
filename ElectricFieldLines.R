0e22a77ead0b95fd5970e12d18890d4df2549a13
jaivrat <jaivrat.singh@gmail.com>
2018-02-06 23:30:41 +0800

    under dev

--- ElectricFieldLines.R
+++ ElectricFieldLines.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 D = 5
 R = 0.5
 epsilon <- 8.85e-12
@@ -10,22 +12,20 @@ Q2 <- -Q
 #Q2 <- -4*Q
 
 
-V <- function(x,y, Q1, Q2)
-{
-  v1 <- function(x,y)
-  {
-    K * Q1 /sqrt(x*x + y*y)
-  }
-  
-  v2 <- function(x,y)
-  {
-    K * Q2 /sqrt((D-x)^2 + y*y)
-  }
-  
-  v1(x,y) + v2(x,y)
-  
-  
-}
+#V <- function(x,y, Q1, Q2)
+#{
+#  v1 <- function(x,y)
+#  {
+#    K * Q1 /sqrt(x*x + y*y)
+#  }
+#  
+#  v2 <- function(x,y)
+#  {
+#    K * Q2 /sqrt((D-x)^2 + y*y)
+#  }
+#  
+#  v1(x,y) + v2(x,y)
+#}
 
 V_grad <- function(x,y, Q1, Q2)
 {
@@ -55,18 +55,18 @@ E <- function(x,y, Q1, Q2)
    list(E_x=-grads$V_grad_x, E_y=-grads$V_grad_y)
 }
 
-E.old <- function(x,y, Q1, Q2)
-{
-  eps <- 1e-4
-  E_x = -(V(x + eps, y, Q1, Q2) - V(x - eps, y, Q1, Q2))/(2 * eps)
-  E_y = -(V(x, y + eps, Q1, Q2) - V(x, y - eps, Q1, Q2))/(2 * eps)
-  list(E_x=E_x, E_y=E_y)
-}
+#E.old <- function(x,y, Q1, Q2)
+#{
+#  eps <- 1e-4
+#  E_x = -(V(x + eps, y, Q1, Q2) - V(x - eps, y, Q1, Q2))/(2 * eps)
+#  E_y = -(V(x, y + eps, Q1, Q2) - V(x, y - eps, Q1, Q2))/(2 * eps)
+#  list(E_x=E_x, E_y=E_y)
+#}
 
 #Directions make sense
-E(4,0, Q1, Q2); E.old(4,0, Q1, Q2)
-E(9,0, Q1, Q2); E.old(9,0, Q1, Q2)
-E(-4,0, Q1, Q2); E.old(-4,0, Q1, Q2)
+E(4,0, Q1, Q2); #E.old(4,0, Q1, Q2)
+E(9,0, Q1, Q2); #E.old(9,0, Q1, Q2)
+E(-4,0, Q1, Q2); #E.old(-4,0, Q1, Q2)
 E(0,4, Q1, Q2)
 
 #at what theta, the angle is 60deg
@@ -84,6 +84,7 @@ bisection <- function(a,b, angle)
   {
     return(a)
   }
+  
   if(tan(b) == obj)
   {
     return(b)
@@ -113,7 +114,7 @@ bisection <- function(a,b, angle)
 }
 
 
-angle = pi/3
+#angle = pi/3
 getFieldPoints <- function(angle)
 {
   #At this point angle is 60
@@ -133,6 +134,224 @@ getFieldPoints <- function(angle)
     theta <- atan(eField$E_y/eField$E_x)
     x_new <- x + ds * cos(theta)
     y_new <- y + ds * sin(theta)
+    return(c(x_new, y_new))
+  }
+  
+  CheckEntry <- function(x,y)
+  {
+    ((x - D)^2 + (y -0)^2 < R*R) || (x >= D + 5*R)
+  }
+  
+  getPoints <- function(x0,y0)
+  {
+    entered = FALSE
+    x = x0; y = y0;
+    df <- data.frame(x=x0,y=y0)
+    while(!entered)
+    {
+      #print(sprintf("getPoints: X=%s, Y=%s\n", x,y))
+      pt <- getNewCord(x,y)
+      df <- rbind(df,c(pt[1], pt[2]))
+      x = pt[1]; y = pt[2]
+      entered = CheckEntry(x, y)
+    }
+    df
+  }
+  
+  #debug(getPoints)
+  ptdf <- getPoints(x0,y0)
+  ptdf[["TYPE"]] <- sprintf("%s", round(angle * 180/pi,2))
+  
+  ptdf
+}
+
+
+dataDF <- rbind(getFieldPoints(0),
+                getFieldPoints(pi/6),
+                getFieldPoints(pi/4),
+                getFieldPoints(pi/3))
+
+# Extend the regression lines beyond the domain of the data
+ggplot(dataDF, aes(x=x, y=y, color=TYPE)) + geom_point(shape=1) +
+  scale_colour_hue(l=50)  # Use a slightly darker palette than normal
+
+
+#add another circle
+getCircle <- function(radius, center)
+{
+  xs <- seq(from = center[1] - radius, to = center[1] + radius, length.out = 1000)
+  ys <- sqrt( radius*radius - (xs - center[1])^2)
+  ysPos <- ys + center[2]
+  ysNeg <- -ys + center[2]
+  posDF <- data.frame(x = xs, y = ysPos)
+  negDF <- data.frame(x = xs, y = ysNeg)
+  return(rbind(posDF, negDF))
+}
+
+
+circdf1 <- getCircle(R, c(0,0))
+circdf1[["TYPE"]] <- "sphere1"
+circdf2 <- getCircle(R, c(D,0))
+circdf2[["TYPE"]] <- "sphere2"
+
+allDF <- rbind(dataDF, circdf1, circdf2)
+
+
+
+# Extend the regression lines beyond the domain of the data
+ggplot(allDF, aes(x=x, y=y, color=TYPE)) + geom_point(shape='....') +
+  scale_colour_hue(l=50)   # Use a slightly darker palette than normal
+
+
+#Specifically for 60deg : When charge equal
+#debug(getFieldPoints)
+df60 <- getFieldPoints(pi/4)
+lastpoint <- df60[dim(df60)[1],]
+fields <- E(lastpoint$x,lastpoint$y, Q1, Q2)
+atan(abs(fields$E_y/fields$E_x)) * 180/pi
+
+# Extend the regression lines beyond the domain of the data
+ggplot(df60, aes(x=x, y=y, color=TYPE)) + geom_point(shape='....') +
+  scale_colour_hue(l=50)   # Use a slightly darker palette than normal
+
+
+
+library(ggplot2)
+
+D = 5
+R = 0.5
+epsilon <- 8.85e-12
+#pi
+#K = 1/(4*pi*epsilon)
+K = 1
+Q <- 100
+Q1 <- 4*Q
+Q2 <- -Q
+#Q2 <- -4*Q
+
+
+#V <- function(x,y, Q1, Q2)
+#{
+#  v1 <- function(x,y)
+#  {
+#    K * Q1 /sqrt(x*x + y*y)
+#  }
+#  
+#  v2 <- function(x,y)
+#  {
+#    K * Q2 /sqrt((D-x)^2 + y*y)
+#  }
+#  
+#  v1(x,y) + v2(x,y)
+#}
+
+V_grad <- function(x,y, Q1, Q2)
+{
+  v1_grad <- function(x,y)
+  {
+    list(dvx = - K * Q1 * x * (x^2 + y^2)^(-3/2), dvy = - K * Q1 * y * (x^2 + y^2)^(-3/2))
+  }
+  
+  v2_grad <- function(x,y)
+  {
+    list(dvx = K * Q2 * (D - x) * ((D-x)^2 + y^2)^(-3/2),  dvy = -K * Q2 * y *  ((D-x)^2 + y^2)^(-3/2))
+  }
+  
+  v1_grad_res = v1_grad(x,y)
+  v2_grad_res = v2_grad(x,y)
+  
+  list(V_grad_x = v1_grad_res$dvx + v2_grad_res$dvx,  V_grad_y = v1_grad_res$dvy + v2_grad_res$dvy)
+}
+
+E <- function(x,y, Q1, Q2)
+{
+  #eps <- 1e-4
+  #E_x = -(V(x + eps, y, Q1, Q2) - V(x - eps, y, Q1, Q2))/(2 * eps)
+  #E_y = -(V(x, y + eps, Q1, Q2) - V(x, y - eps, Q1, Q2))/(2 * eps)
+  #list(E_x=E_x, E_y=E_y)
+  grads = V_grad(x,y, Q1, Q2)
+  list(E_x=-grads$V_grad_x, E_y=-grads$V_grad_y)
+}
+
+#E.old <- function(x,y, Q1, Q2)
+#{
+#  eps <- 1e-4
+#  E_x = -(V(x + eps, y, Q1, Q2) - V(x - eps, y, Q1, Q2))/(2 * eps)
+#  E_y = -(V(x, y + eps, Q1, Q2) - V(x, y - eps, Q1, Q2))/(2 * eps)
+#  list(E_x=E_x, E_y=E_y)
+#}
+
+#Directions make sense
+E(4,0, Q1, Q2); #E.old(4,0, Q1, Q2)
+E(9,0, Q1, Q2); #E.old(9,0, Q1, Q2)
+E(-4,0, Q1, Q2); #E.old(-4,0, Q1, Q2)
+E(0,4, Q1, Q2)
+
+#at what theta, the angle is 60deg
+theta1 = 0
+theta2 = pi/2
+
+bisection <- function(a,b, angle)
+{
+  obj   = tan(angle)
+  
+  if( (tan(b) - obj) * (tan(a) - obj) > 0)
+    stop("Wrong choice of intervals")
+  
+  if(tan(a) == obj)
+  {
+    return(a)
+  }
+  
+  if(tan(b) == obj)
+  {
+    return(b)
+  }
+  
+  mn = min(a,b)
+  mx = max(a,b)
+  a = mn; b = mx
+  #initial case
+  theta = (a + b)/2
+  if(abs(tan(theta) - obj) < 1e-10)
+  {
+    return(theta)
+  }
+  
+  while(abs(tan(theta) - obj) > 1e-10)
+  {
+    print(sprintf("bisection: a=%s b=%s, theta = %s\n", a*180/pi, b*180/pi, theta * 180/pi))
+    if( (tan(theta) - obj) * (tan(a) - obj) < 0 ){
+      b = theta
+    } else if ( (tan(theta) - obj) * (tan(b) - obj) < 0 ) {
+      a = theta
+    }
+    theta = (a + b)/2
+  }
+  return(theta)
+}
+
+
+#angle = pi/3
+getFieldPoints <- function(angle)
+{
+  #At this point angle is 60
+  theta = bisection(0, pi/2, angle)
+  print(sprintf("getFieldPoints: Angle = %s", theta * 180/pi))
+  x0 = R * cos(theta)
+  y0 = R * sin(theta)
+  eField = E(x0,y0, Q1, Q2)
+  print(eField)
+  print(sprintf("getFieldPoints: Angle of field = %s degrees" , atan(eField$E_y/eField$E_x) * 180/pi))
+  
+  #from this we find the curve
+  getNewCord <- function(x,y)
+  {
+    ds <- 0.1
+    eField <- E(x,y, Q1, Q2)
+    theta <- atan(abs(eField$E_y/eField$E_x))
+    x_new <- x + ds * cos(theta) * sign(eField$E_x)
+    y_new <- y + ds * sin(theta) * sign(eField$E_y)
     return(c(x_new, y_new))
   }
   
@@ -196,18 +415,38 @@ circdf2[["TYPE"]] <- "sphere2"
 allDF <- rbind(dataDF, circdf1, circdf2)
 
 
-library(ggplot2)
+
 # Extend the regression lines beyond the domain of the data
 ggplot(allDF, aes(x=x, y=y, color=TYPE)) + geom_point(shape='....') +
   scale_colour_hue(l=50)   # Use a slightly darker palette than normal
 
 
 #Specifically for 60deg : When charge equal
-df60 <- getFieldPoints(pi/3)
-lastpoint <- df60[dim(df60)[1],]
+#debug(getFieldPoints)
+df45 <- getFieldPoints(pi/4)
+lastpoint <- df60[dim(df45)[1],]
 fields <- E(lastpoint$x,lastpoint$y, Q1, Q2)
 atan(abs(fields$E_y/fields$E_x)) * 180/pi
 
+# Extend the regression lines beyond the domain of the data
+ggplot(df45, aes(x=x, y=y, color=TYPE)) + geom_point(shape='....') +
+  scale_colour_hue(l=50)   # Use a slightly darker palette than normal
+
+
+do.call(rbind, apply(df45[,c("x","y")], 1, function(xy){ c(xy[1], xy[2], E(x=xy[1],y=xy[2], Q1, Q2))}))
+
+#           x         y         E_x         E_y
+#62  5.162707  1.330088  6.866021    -51.76825 
+#63  5.175855  1.230956  4.601721    -60.75577 
+#64  5.183408  1.131242  1.698451    -72.12982 
+#65  5.185762  1.03127   -2.111424   -86.83935 
+#66  5.188192  1.13124   1.381988    -71.98568 
+#67  5.190112  1.031259  -2.475341   -86.64812 
+#68  5.192967  1.131218  1.067444    -71.84108 
+
+row = 186
+field <- E(x=df45[row,"x"],y= df45[row,"y"], Q1, Q2); field
+atan(field$E_y/field$E_x) * 180/pi; sin(atan(field$E_y/field$E_x) )
 
 
 
