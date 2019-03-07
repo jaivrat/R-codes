5f8c2df76215bbae5d1de76c9f8a8a6c2ebb0ebc
jaivrat <jaivrat.singh@gmail.com>
2019-03-08 07:35:08 +0800

    some code cleanup

--- PCA2.R
+++ PCA2.R
@@ -1,4 +1,4 @@
-#data I gerenaretd using https://www.librec.net/datagen.html, at approx 45 dgrees
+#data I gerenaretd using https://www.librec.net/datagen.html, at approx 45 dgrees : Will help verify components
 df <- read.csv(text = 
 "x,y,lab
 3, 3.05, 1
@@ -33,23 +33,34 @@ df <- read.csv(text =
 6, 4.35, 1
 5.6, 5.15, 1", header = TRUE)
 
+# Plot to have a sense -------------------------------------------------
 head(df)
 data.matrix <- as.matrix(df[,c("x","y")])
-#data.matrix <- scale(data.matrix)
 rownames(data.matrix) <- paste("Sample-", 1:dim(data.matrix)[1], sep="")
+#data.matrix <- scale(data.matrix)
+plot(data.matrix[,"x"], data.matrix[,"y"])
+
+
+# PCA using prcomp -----------------------------------------------------
 pca         <- prcomp(data.matrix, scale= FALSE, center = FALSE)
+
+# PCA outputs (sdev, x, rotation)---------------------------------------
+# sdev: standard deviation along component axis
 pca$sdev
+(pca.var <- pca$sdev^2)
+(pca.var.per <- round(pca.var/sum(pca.var) * 100, 1))
+barplot(pca.var.per, xlab = "principal component", ylab="% variation")
+# x: pricinpal components : (ie for each sample, we have transformed cordinates along PC1, PC2, ... PCN)
 pca$x
 plot(pca$x[,1], pca$x[,2])
-pca.var <- pca$sdev^2
-pca.var.per <- round(pca.var/sum(pca.var) * 100, 1)
-pca.var.per
-barplot(pca.var.per, xlab = "principal component", ylab="% variation")
-
+# rotation: the columns are PC directions. columns are unit vectors along PC directions. THey also will be orthogonal
+pca$rotation
+apply( pca$rotation, 2, function(e) sum(e^2))#All must sum to 1
+#some call these PC vector components as loading scores, because you can take component of original 
+#data with loading scores(as a vector dot product and get transfomed vector in terms of PC directions)
+loading_scores <- pca$rotation[,1]; loading_scores
+#Plot PCA transformed PCA data
 pca.data <- data.frame(Sample=rownames(pca$x), X = pca$x[,1], Y = pca$x[,2])
-pca.data
-
-
 ggplot(data = pca.data, aes(x = X, y = Y, label=Sample)) + 
   geom_point() + 
   #geom_text() + 
@@ -58,24 +69,20 @@ ggplot(data = pca.data, aes(x = X, y = Y, label=Sample)) +
   theme_bw() + 
   ggtitle("My PCA graph")
 
-loading_scores <- pca$rotation[,1]; loading_scores
-#All must sum to 1
-apply( pca$rotation, 2, function(e) sum(e^2))
-
 #if we talke original data series and take dot product of each row on PC1 pf rotation, we will get 
 projections = data.matrix %*% pca$rotation
-#means <- apply(projections,2, mean)
-#head(apply(projections, 2, function(e) e - means))
 head(projections)
-head(pca$x)
+head(pca$x) #This matches with above if scale=FALSE, center = FALSE in prcomp. or data.matrix not scaled(cetered + scaled)
 
+
+
+#SCALE = TRUE ----------------------------------------------------------
 ##If we use ceter and scaling, then data matrix is scaled and sentered before processing.
 pca         <- prcomp(data.matrix, scale= TRUE)
 projections = data.matrix %*% pca$rotation
 #These wont be same, the two below: because data matrix is scaled and sentered before processing.
 head(projections)
 head(pca$x)
-
 projections.sc = scale(data.matrix) %*% pca$rotation
 #Now these will be equal
 head(projections.sc)
